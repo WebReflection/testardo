ca0eb352270d07f36afc2dd18e0ed89bb07b333e
Andrea Giammarchi <agiammarchi@twitter.com>
2014-01-31 13:02:26 -0800

    simplified the example and added a jQuery.ready one

--- examples/ok.js
+++ examples/ok.js
@@ -1,9 +1,7 @@
 // it should do nothing but showing OK
 module.exports = {
   path: 'about:blank',
-  external: ['http://code.jquery.com/jquery-2.0.3.js'],
   test: function(sandbox, window, document) {
-    var $ = window.jQuery;
     sandbox.done();
   }
 };
